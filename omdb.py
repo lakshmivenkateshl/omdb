468af0c7d3eeb1e14fc2587e7f22ec1c9949e3d8
lakshmivenkateshl <51985477+lakshmivenkateshl@users.noreply.github.com>
2019-06-20 12:58:54 +0530

    Update omdb.py

--- omdb.py
+++ omdb.py
@@ -1,10 +1,34 @@
-import omdb
-omdb.set_default('apikey', API_KEY)
-client = omdb.OMDBClient(apikey=API_KEY)
-omdb.get(title='True Grit', year=1969, fullplot=True, tomatoes=True)
-omdb.get(title='True Grit', year=1969, fullplot=True, tomatoes=True, timeout=5)
-omdb.search('True Grit') 
-omdb.search('True Grit', timeout=5) 
-omdb.search('true', page=2)
+import requests
+import json
+API_KEY = 'YOUR_KEY'
+OMDB_URL = 'http://www.omdbapi.com/?t=iron+man&plot=full' + API_KEY
+def get_data(url):
+
+    data = json.loads(requests.get(url).text)
+
+    if data['Response'] == 'True':
+
+        return data
+
+    else:
+
+        return None
+
+def search_Titles_by_keyword(keywords):
+
+    query = '+'.join(keywords.split())  # e.g., "Iron Man" -> Iron+Man
+
+    url = OMDB_URL + '&s=' + query
+
+    data = get_data(url)
+
+    return data
+
+if __name__ == '__main__':
+
+    keyword = 'iron man'
+
+    data = search_Titles_by_keyword(keyword)
+    print(data) 
 
 
